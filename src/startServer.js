4582cb6a72f84a2c7ef9781707575286e5e8b660
AdaoBJr <adaobjr@gmail.com>
2022-01-01 13:45:43 -0400

    feat: :sparkles: added new instance sequelize

--- src/startServer.js
+++ src/startServer.js
@@ -1,8 +1,15 @@
 import { ApolloServer } from 'apollo-server';
+import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
 export default function startServer({ typeDefs, resolvers }) {
   const { SERVER_PORT } = process.env;
+
+  const sequelize = new Sequelize('test-db', 'user', 'pass', {
+    dialect: 'sqlite',
+    host: './dev.slqlite',
+  });
+  sequelize.sync().then(() => console.log('🎲 db is running'));
 
   const app = new ApolloServer({
     typeDefs,
